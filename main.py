7b40e709fc95158562639a9cdb9dc257ed85d29e
Confused-sounds <79672020+Confused-sounds@users.noreply.github.com>
2023-06-22 02:59:45 +0000

    Fonts are cringe

--- main.py
+++ main.py
@@ -1,12 +1,28 @@
 import tkinter as tk
+from tkinter import ttk
+import tkinter.font
+def _innit_():
+    Menu = tk.Tk()
+    Menu.title("Gam")
+    Menu.geometry("723x373")
 
-window = tk.Tk()
-window.title("gey?")
-window.geometry("300x300")
 
-hello = tk.Label(text="y r u gey?")
-hello.pack()
-button = tk.Button(text="Im not gey")
-button.pack()
+    hello = ttk.Label(text="Main Menu")
+    Desired_font = tkinter.font.Font(family = "Comic Sans MS", size = 10)
+    hello.place(relx = .5, rely = .1, anchor="n",)
+    hello.configure(font = Desired_font)
+    
+    button = ttk.Button(text="Settings")
+    button.place(relx = .5, rely = .5, anchor="center")
 
+
+
+
+
+
+
+
+
+
+_innit_()
 tk.mainloop()
